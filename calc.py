c4c67dc17385a2c5ad0f1f27521e46ed1b283f76
AGKirby <adamgraneto2@gmail.com>
2021-04-15 16:51:54 -0700

    updated calculator functionality

--- calc.py
+++ calc.py
@@ -1,43 +1,19 @@
 def calc():
+    # get user input
     try: 
         num1 = int(input("Please enter the first number: ")) # get a number from the user
-        operation = input("Please enter the operation (+, -, *, /): ") # get a character from the user
         num2 = int(input("Please enter the second number: ")) # get a number from the user
     except: # if the user did not enter an integer
         print("Invalid input: integer expected. Exiting program.")
         return
     result = 0
-    # call a function based on the inputted operation
-    if(operation == "+"):
-        result = add(num1, num2)
-    elif(operation == "-"):
-        result = subract(num1, num2)
-    elif(operation == "*"):
-        result = multiply(num1, num2)
-    elif(operation == "/"):
-        try: 
-            result = divide(num1, num2)
-        except: # if divide by zero error
-            print("Invalid divisor: the second number cannot be zero in a division statement!")
-            return
-    else: #if inputted operation is not valid
-        print("Invalid operation: expected one of the follow: '+', '-', '*', or '/'")
-        return
-    # print the results
-    print(str(num1) + " " + operation + " " + str(num2) + " = " + str(result))
-    #change made
-
-
-def add(x, y):
-    return x + y
-
-def subtract(x, y):
-    return x - y
-
-def multiply(x, y):
-    return x * y
-
-def divide(x, y):
-    return x / y
+    # calculate the results
+    sum = num1 + num2
+    difference = num1 - num2
+    multiply = num1 * num2
+    try: 
+        divide = num1 / num2
+    except: #divide by zero error!
+        divide = None
 
 calc()
