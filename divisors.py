98455cf65e41465ba23eaba03655c46c7288a02d
AGKirby <adamgraneto2@gmail.com>
2021-04-15 17:12:12 -0700

    Made the printed output look better with commas and the word and

--- divisors.py
+++ divisors.py
@@ -4,10 +4,17 @@ def divisors():
     except: # if the user did not enter an integer
         print("Invalid input: integer expected. Exiting program.")
         return
-    print(str(num) + " is divisible by ")
+    print(str(num) + " is divisible by ", end="")
+    first = True
     for i in range(1, num//2+1): # numbers from one to half of the user's input plus one
         if(num % i == 0): #divisible by i
-            print(str(i), end=" ")
+            if(not first): #if not the first number then add a comma
+                print(", ", end="")
+            else: 
+                first = False
+            print(str(i), end="")
+    if(not first):
+        print(" and ", end="")
     print(str(num))
 
 
