5c0e869edda9a0df6d185e9c0e31e75e618ef581
AGKirby <adamgraneto2@gmail.com>
2021-04-15 17:08:10 -0700

    Added collection of user input

--- divisors.py
+++ divisors.py
@@ -1,4 +1,9 @@
 def divisors():
+    try: 
+        num = int(input("Please enter the first number: ")) # get a number from the user
+    except: # if the user did not enter an integer
+        print("Invalid input: integer expected. Exiting program.")
+        return
     pass
 
 
