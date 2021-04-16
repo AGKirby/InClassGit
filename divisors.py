40562dd32bf1be8c62ef673637196a038ac5acff
AGKirby <adamgraneto2@gmail.com>
2021-04-15 17:15:34 -0700

    Added collection of user input

--- divisors.py
+++ divisors.py
@@ -1,6 +1,6 @@
 def divisors():
     try: 
-        num = int(input("Please enter the first number: ")) # get a number from the user
+        num = int(input("Please enter the number: ")) # get a number from the user
     except: # if the user did not enter an integer
         print("Invalid input: integer expected. Exiting program.")
         return
--- passwordGenerator.py
+++ passwordGenerator.py
@@ -1,5 +1,9 @@
 def generatePassword():
-    pass
+    try: 
+        num = int(input("Please enter the length of the password to be generated: ")) # get a number from the user
+    except: # if the user did not enter an integer
+        print("Invalid input: integer expected. Exiting program.")
+        return
 
 
 
