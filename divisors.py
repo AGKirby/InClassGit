2f3b771d8cf90f18e444738881cde2d0f04c718e
AGKirby <adamgraneto2@gmail.com>
2021-04-15 17:10:57 -0700

    Added print statement

--- divisors.py
+++ divisors.py
@@ -4,6 +4,7 @@ def divisors():
     except: # if the user did not enter an integer
         print("Invalid input: integer expected. Exiting program.")
         return
+    print(str(num) + " is divisible by ")
     for i in range(1, num//2+1): # numbers from one to half of the user's input plus one
         if(num % i == 0): #divisible by i
             print(str(i), end=" ")
