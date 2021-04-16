7fbe50b341911ece605095392c2d81a7d756ba63
AGKirby <adamgraneto2@gmail.com>
2021-04-15 17:16:46 -0700

    Added creation of the password

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -1,9 +1,12 @@
 def generatePassword():
     try: 
-        num = int(input("Please enter the length of the password to be generated: ")) # get a number from the user
+        length = int(input("Please enter the length of the password to be generated: ")) # get a number from the user
     except: # if the user did not enter an integer
         print("Invalid input: integer expected. Exiting program.")
         return
+    password = []
+    for i in range(length):
+        password += "A"
 
 
 
